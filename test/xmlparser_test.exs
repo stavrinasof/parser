7ec54133d8253ed8c0d09655b15d7cd48040cb2f
alexalepis <aalepis@NT-21.tcb.local>
2018-03-30 20:53:05 +0300

    minor test fix

--- test/xmlparser_test.exs
+++ test/xmlparser_test.exs
@@ -8,10 +8,10 @@ defmodule XmlParserTest do
     Enum.all?(event_ids, fn x -> String.contains?(xml, to_string(x))end)
   end
 
-  test "check if naive_map has" do
+  test "check if naive_map has correct structure and certain key-value is correct" do
     
     map = XmlParser.with_xmltomap
-    pl1_id = get_in(map, [{"ContentAPI"}, {"Sport", 'TENN'}, {"SBClass", '10009'}, {"SBType", '17632'}, {"Ev", '1680638'}, {"Incidents"}, {"Incident", '101687325'}, "player1_id"])
+    pl1_id = get_in(map, [{"ContentAPI"}, {"Sport", "TENN"}, {"SBClass", "10009"}, {"SBType", "17632"}, {"Ev", "1680638"}, {"Incidents"}, {"Incident", "101687325"}, "player1_id"])
 
     assert pl1_id=="1005838"
   end
