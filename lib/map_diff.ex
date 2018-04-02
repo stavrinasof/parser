88823cb1e92b46059c276686c1acd6bb89715dc5
m.kousanakis <m.kousanakis@stoiximan.gr>
2018-04-02 12:57:04 +0300

    map diff version 0.2.1

--- lib/map_diff.ex
+++ lib/map_diff.ex
@@ -2,26 +2,28 @@ defmodule MapDiff do
   def diffs(map_a, map_b) when is_map(map_a) and is_map(map_b), do: diffs(map_a, map_b, [], %{:key_doesnt_exist => %{}, :changes => %{}})
 
   defp diffs(map_a, map_b, key_path, acc) when is_map(map_a) and is_map(map_b) do
-    # acc = %{:key_doesnt_exist => %{}, :changes => %{}}
 
     map_b
     |> Map.keys()
-    |> Enum.reduce(acc, fn k, acc ->
+    |> Enum.reduce(acc, fn k, acc -> 
                                     case Map.get(map_a, k) do
-                                      nil         -> Map.update!(acc, :key_doesnt_exist, &Map.put(&1, [k | key_path], Map.get(map_b, k)))
-                                      map_a_value -> result = diffs(Map.get(map_b, k), map_a_value, [k | key_path], acc)
-
-                                        case result do
-                                          {:ok, :equals} -> acc
-                                          {:ok, :change} -> Map.update!(acc, :changes, &Map.put(&1, [k | key_path], Map.get(map_b, k)))
-                                          _              -> result
-                                        end
+                                        nil         -> reversed_new_key_path = [k | key_path] |> Enum.reverse
+                                                       Map.update!(acc, :key_doesnt_exist, &Map.put(&1, reversed_new_key_path, Map.get(map_b, k)))
+                                        map_a_value -> diffs(map_a_value, Map.get(map_b, k), [k | key_path], acc)
+                                                       |> check_diff_state(acc, [k | key_path], Map.get(map_b, k))
                                     end
     end)
-    |> IO.inspect()
   end
+
 
   defp diffs(map_a_value, map_b_value, key_path, _result) when map_a_value == map_b_value, do: {:ok, :equals}
 
   defp diffs(map_a_value, map_b_value, key_path, _result), do: {:ok, :change}
+
+  defp check_diff_state({:ok, :change}, acc, new_key_path, map_b_value) do
+   reversed_new_key_path = new_key_path |> Enum.reverse
+   Map.update!(acc, :changes, &Map.put(&1, reversed_new_key_path, map_b_value))
+  end
+  defp check_diff_state({:ok, :equals}, acc, _,_), do: acc
+  defp check_diff_state(result, _, _,_), do: result
 end
