30a29f4520d44d3d6e826aea7d2c2ace1f85b815
stavrinasof <staurinasof@gmail.com>
2018-04-05 15:28:50 +0300

    tests working for this solution

--- test/xml_to_map_test.exs
+++ test/xml_to_map_test.exs
@@ -23,6 +23,35 @@ defmodule XmlToMaptTest do
               ]}
 
   @notes {'Notes', [], ['Pitchers: Foltynewicz (ATL) - Scherzer (WSH)']}
+  @market {'Mkt',[
+      {'status', 'A'},
+      {'displayed', 'Y'},
+      {'mkt_id', '118360'}
+    ],
+    [
+      @selection,
+      {'Seln',
+              [
+                {'seln_sort', 'H'},
+                {'disporder', '1'},
+                {'team_id', '118360'},
+                {'seln_id', '484026786'},
+                {'name', 'Eastern Lions'},
+                {'status', 'A'}
+              ],
+              [
+                {'Price',
+                 [
+                   {'last_change', '2018-03-28T09:56:47'},
+                   {'us_prc', '+3300'},
+                   {'frac_prc', '33/1'},
+                   {'bet_ref', 'NDg0MDI2NzkzOjMzLzE6OjA'},
+                   {'dec_prc', '34.50'},
+                   {'prc_type', 'LP'}
+                 ], []}
+              ]}
+    ]
+  }
 
   test "do parse attributes into a map" do
     {_tagname, attributes, _content} = @selection
@@ -32,7 +61,7 @@ defmodule XmlToMaptTest do
   end
 
   test "parse tuple returns map" do
-    seln_map = XmlToMap.parse(@selection)
+    seln_map = XmlToMap.parse(@selection,0)
     seln_value_map = Map.get(seln_map, {"Seln", "484026793"})
 
     assert is_map(seln_map) == true
@@ -40,16 +69,27 @@ defmodule XmlToMaptTest do
   end
 
   test "parse notes" do
-    notes_map = XmlToMap.parse(@notes)
+    notes_map = XmlToMap.parse(@notes,0)
     assert is_map(notes_map) == true
     assert Map.keys(notes_map) == ["Notes"]
   end
 
   test "parse turple with tuple content" do
-    seln_map = XmlToMap.parse(@selection)
+    seln_map = XmlToMap.parse(@selection,0)
     seln_value_map = Map.get(seln_map, {"Seln", "484026793"})
 
-    assert length(Map.keys(seln_value_map)) == 7
+    assert length(Map.keys(seln_value_map)) == 8   #7+1 for order attribute
     assert Map.get(seln_value_map, {"Price", "LP"}) != nil
+  end
+
+
+  test "order in attributes" do
+    market_map = XmlToMap.parse(@market,0)
+    #IO.inspect market_map
+    seln_value_map1 = get_in(market_map, [{"Mkt", "118360"},{"Seln", "484026793"}])
+    seln_value_map2 = get_in(market_map, [{"Mkt", "118360"},{"Seln", "484026786"}])
+
+    assert Map.get(seln_value_map1, "order") == "0"
+    assert Map.get(seln_value_map2, "order") == "1"
   end
 end
