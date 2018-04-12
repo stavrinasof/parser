1b137d95268878f6df91f1d6e3eac010141e02fa
stavrinasof <staurinasof@gmail.com>
2018-04-12 16:26:28 +0300

    end of parsing with sax

--- lib/xml_to_map_sax.ex
+++ lib/xml_to_map_sax.ex
@@ -1,13 +1,12 @@
 defmodule XmlToMapSax do
+  @upperclasses ['Sport','SBClass','SBType']
   def saxmap(xml) do
     xml = String.replace(xml, ~r/\sxmlns=\".*\"/, "")
     :erlsom.parse_sax(xml, [], fn xmline,acc -> parse_element(xmline,acc)  end)
     |>elem(1)
-    |>List.first
-    |>elem(1)
+    |>Enum.reduce(%{},fn {_,m}, acc ->Map.merge(m,acc) end )
+    # |>elem(1)
 
-    # |>Enum.reduce([],&my_reduce_fun(&1,&2))
-    # |>Enum.reduce(%{},&Map.merge(&2,&1))
   end
 
   defp parse_attributes([],attrmap) ,do: attrmap
@@ -25,6 +24,22 @@ defmodule XmlToMapSax do
 
   def parse_element({:characters, characters} ,acc_tuples) ,do: [{:chars,to_string(characters)}|acc_tuples]
   def parse_element({:ignorableWhitespace, _} ,acc_tuples) ,do: acc_tuples
+
+  def parse_element({:endElement, [], 'ContentAPI'=tagname, []},acc_tuples) do
+    name =to_string(tagname)
+    index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==name end)
+    Enum.reject(acc_tuples,fn x -> elem(x,0) ==name end)
+  end
+  def parse_element({:endElement, [], tagname, []},acc_tuples) when tagname in @upperclasses do
+    name =to_string(tagname)
+    index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==name end)
+    map_inner=
+      acc_tuples
+      |> Enum.at(index)
+      |> elem(1)
+    rest_tuples= Enum.reject(acc_tuples,fn x -> elem(x,0) ==name end)
+    [{name,%{name=> map_inner}}|rest_tuples]
+  end
   def parse_element({:endElement, [], tagname, []},acc_tuples) do
     name =to_string(tagname)
     index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==name end)
@@ -32,7 +47,6 @@ defmodule XmlToMapSax do
       acc_tuples
       |> Enum.take(index+1)
       |> Enum.reduce(%{},fn {_,m} , acc -> MapActions.dynamic_merge(m,acc) end)
-     # |> IO.inspect
     rest_tuples= Enum.drop(acc_tuples,index+1)
     [{name,%{name=> map_inner}}|rest_tuples]
   end
