7ad9042b89a7b1ceb6b10ecf9b5737951a04ea92
stavrinasof <staurinasof@gmail.com>
2018-04-26 10:17:46 +0300

    fix(xmltosax): no usage of strings

--- lib/xml_to_map_sax.ex
+++ lib/xml_to_map_sax.ex
@@ -11,7 +11,7 @@ defmodule XmlToMapSax do
 
   defp parse_attributes([],attrmap) ,do: attrmap
   defp parse_attributes([{:attribute, attrname, [], [], attrvalue}|attrlist],attrmap) do
-    attrmap = Map.put(attrmap,to_string(attrname),to_string(attrvalue))
+    attrmap = Map.put(attrmap,attrname,attrvalue)
     parse_attributes(attrlist,attrmap)
   end
 
@@ -19,35 +19,34 @@ defmodule XmlToMapSax do
   def parse_element(:startDocument,[]) ,do: []
   def parse_element(:endDocument,acc_tuples) ,do: acc_tuples
   def parse_element({:startElement, [], tagname, [], attrlist} ,acc_tuples) do
-    [{to_string(tagname),parse_attributes(attrlist,%{})} |acc_tuples ]
+    [{tagname,parse_attributes(attrlist,%{})} |acc_tuples ]
   end
 
-  def parse_element({:characters, characters} ,acc_tuples) ,do: [{:chars,to_string(characters)}|acc_tuples]
+  def parse_element({:characters, characters} ,acc_tuples) ,do: [{:chars,characters}|acc_tuples]
   def parse_element({:ignorableWhitespace, _} ,acc_tuples) ,do: acc_tuples
 
   def parse_element({:endElement, [], 'ContentAPI'=tagname, []},acc_tuples) do
-    name =to_string(tagname)
-    index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==name end)
-    Enum.reject(acc_tuples,fn x -> elem(x,0) ==name end)
+
+    # index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==name end)
+    Enum.reject(acc_tuples,fn x -> elem(x,0) ==tagname end)
   end
   def parse_element({:endElement, [], tagname, []},acc_tuples) when tagname in @upperclasses do
-    name =to_string(tagname)
-    index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==name end)
+    index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==tagname end)
     map_inner=
       acc_tuples
       |> Enum.at(index)
       |> elem(1)
-    rest_tuples= Enum.reject(acc_tuples,fn x -> elem(x,0) ==name end)
-    [{name,%{name=> map_inner}}|rest_tuples]
+    rest_tuples= Enum.reject(acc_tuples,fn x -> elem(x,0) ==tagname end)
+    [{tagname,%{tagname=> map_inner}}|rest_tuples]
   end
   def parse_element({:endElement, [], tagname, []},acc_tuples) do
-    name =to_string(tagname)
-    index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==name end)
+    # name =tagname
+    index= Enum.find_index(acc_tuples, fn x -> elem(x,0) ==tagname end)
     map_inner=
       acc_tuples
       |> Enum.take(index+1)
       |> Enum.reduce(%{},fn {_,m} , acc -> MapActions.dynamic_merge(m,acc) end)
     rest_tuples= Enum.drop(acc_tuples,index+1)
-    [{name,%{name=> map_inner}}|rest_tuples]
+    [{tagname,%{tagname=> map_inner}}|rest_tuples]
   end
 end
